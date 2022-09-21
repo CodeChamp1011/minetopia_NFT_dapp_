dbd033213f0e0d12f2ecece4be8f4d1e1180e9f0
Kenny <technobing@gmail.com>
2022-09-21 22:59:52 +0800

    eth value change

--- src/App.js
+++ src/App.js
@@ -189,7 +189,7 @@ function App() {
 						clientId="4a79af24-12f9-4c22-b364-f9ff2d3b3df8"
 						mintConfig={{
 							"type":"erc-721",
-							"totalPrice": "0.192"
+							"totalPrice": "0.187"
 						}}
 					/> 
 				</div>
